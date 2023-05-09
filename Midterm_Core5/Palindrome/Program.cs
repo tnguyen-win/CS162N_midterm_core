5984f5f4affd629d1fc1b961d4b15c5fb322274a
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-05-09 12:31:00 -0700

    Midterm - Palindrome

--- Midterm_Core5/Palindrome/Program.cs
+++ Midterm_Core5/Palindrome/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Palindrome
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace Palindrome {
+	class Program {
+		static void Main() {
+			Console.Write("Enter a word/phrase to see if it's a palindrome: ");
+
+			string promptText = Console.ReadLine();
+			string finalText = "";
+
+			foreach (char s in promptText) if (!char.IsPunctuation(s) && !char.IsWhiteSpace(s)) finalText += s.ToString().ToLower();
+
+			promptText = finalText;
+
+			finalText = IsPalindrome(finalText);
+
+			Console.WriteLine($"\n\nThe word/phrase you entered is a palindrome : {promptText == finalText}\n");
+		}
+
+		static string IsPalindrome(string finalText) {
+			return finalText.Length > 0 ? finalText[^1] + IsPalindrome(finalText[0..^1]) : finalText;
+		}
+	}
 }
