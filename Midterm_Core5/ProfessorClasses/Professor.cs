463b0b7b3378c1dd91d2b0da35d23b5a1f2ebe26
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-05-09 12:32:24 -0700

    Midterm - Professor/Student

--- Midterm_Core5/ProfessorClasses/Professor.cs
+++ Midterm_Core5/ProfessorClasses/Professor.cs
@@ -1,8 +1,53 @@
 ﻿using System;
 
-namespace ProfessorClasses
-{
-    public class Professor
-    {
-    }
+namespace ProfessorClasses {
+	public class Student {
+		private string lNumber;
+		private string nameFirst;
+		private string nameLast;
+		private string major;
+		private string birthDate;
+
+		public Student() => lNumber = nameFirst = nameLast = major = birthDate;
+
+		public Student(string studentLNUMBER, string studentNameFirst, string studentNameLast, string studentMajor, string studentBirthDate) {
+			lNumber = studentLNUMBER;
+			nameFirst = studentNameFirst;
+			nameLast = studentNameLast;
+			major = studentMajor;
+			birthDate = studentBirthDate;
+		}
+
+		public string LNUMBER {
+			get => lNumber;
+			set {
+				if (value.Length == 9) lNumber = value;
+				else throw new ArgumentException("LNUMBER value must be exactly 9 characters long.");
+			}
+		}
+
+		public string NameFirst {
+			get => nameFirst;
+			set => nameFirst = value;
+		}
+
+		public string NameLast {
+			get => nameLast;
+			set => nameLast = value;
+		}
+
+		public string Major {
+			get => major;
+			set => major = value;
+		}
+
+		public string BirthDate {
+			get => birthDate;
+			set => birthDate = value;
+		}
+
+		public override string ToString() {
+			return $"LNUMBER: [{lNumber}] | NameFirst: [{nameFirst}] | NameLast: [{nameLast}] | Major: [{major}] | BirthDate: [{birthDate}]";
+		}
+	}
 }
--- Midterm_Core5/ProfessorTests/Program.cs
+++ Midterm_Core5/ProfessorTests/Program.cs
@@ -1,14 +1,105 @@
 ﻿using System;
-
 using ProfessorClasses;
 
-namespace ProfessorTests
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace ProfessorTests {
+	class Program {
+		static void Main() {
+			TestStudentConstructors();
+			TestStudentPropertyGetters();
+			TestStudentPropertySetters();
+			TestStudentPropertySettersWithExceptions();
+			TestStudentToString();
+		}
+
+		static void TestStudentConstructors() {
+			Student s1 = new();
+			Student s2 = new("L01234567", "John", "Smith", "Computer Programming", "1/01/1991");
+
+			Console.WriteLine("[Testing both constructors]");
+
+			Console.WriteLine("Default constructor -");
+			Console.WriteLine("Expecting: default values (LNUMBER: [] | NameFirst: [] | NameLast: [] | Major: [] | BirthDate: [])");
+			Console.WriteLine($"Got: {s1}");
+
+			Console.WriteLine("Overloaded constructor -");
+			Console.WriteLine("Expecting: LNUMBER: [L01234567] | NameFirst: [John] | NameLast: [Smith] | Major: [Computer Programming] | BirthDate: [1/01/1991]");
+			Console.WriteLine($"Got: {s2}\n\n");
+		}
+
+		static void TestStudentPropertyGetters() {
+			Student s = new("L01234567", "John", "Smith", "Computer Programming", "1/01/1991");
+
+			Console.WriteLine("[Testing getters]");
+
+			Console.WriteLine($"LNUMBER -");
+			Console.WriteLine($"Expecting: [L01234567]");
+			Console.WriteLine($"Got: [{s.LNUMBER}]");
+
+			Console.WriteLine($"NameFirst -");
+			Console.WriteLine($"Expecting: [John]");
+			Console.WriteLine($"Got: [{s.NameFirst}]");
+
+			Console.WriteLine($"NameLast -");
+			Console.WriteLine($"Expecting: [Smith]");
+			Console.WriteLine($"Got: [{s.NameLast}]");
+
+			Console.WriteLine($"Major -");
+			Console.WriteLine($"Expecting: [Computer Programming]");
+			Console.WriteLine($"Got: [{s.Major}]");
+
+			Console.WriteLine($"BirthDate -");
+			Console.WriteLine($"Expecting: [1/01/1991]");
+			Console.WriteLine($"Got: [{s.BirthDate}]\n\n");
+		}
+
+		static void TestStudentPropertySetters() {
+			Student s = new("L01234567", "John", "Smith", "Computer Programming", "1/01/1991");
+
+			Console.WriteLine("[Testing setters]");
+
+			s.LNUMBER = "L76543210";
+			s.NameFirst = "Jane";
+			s.NameLast = "Doe";
+			s.Major = "Cybersecurity";
+			s.BirthDate = "12/12/2012";
+
+			Console.WriteLine("Expecting: LNUMBER: [L76543210] | NameFirst: [Jane] | NameLast: [Doe] | Major: [Cybersecurity] | BirthDate: [12/12/2012]");
+			Console.WriteLine($"Got: {s}\n\n");
+		}
+
+		static void TestStudentPropertySettersWithExceptions() {
+			Student s = new("L01234567", "John", "Smith", "Computer Programming", "1/01/1991");
+
+			Console.WriteLine("[Testing invalid setter values]");
+
+			try {
+				s.LNUMBER = "1";
+			}
+			catch {
+				Console.WriteLine("(LNUMBER = 1) Threw an exception when [LNUMBER] wasn't exactly 9 characters long -");
+				Console.WriteLine($"Expecting: [L01234567]");
+				Console.WriteLine($"Got: [{s.LNUMBER}]");
+			}
+
+			try {
+				s.LNUMBER = "0123456789";
+			}
+			catch {
+				Console.WriteLine("(LNUMBER = 0123456789) Threw an exception when [LNUMBER] wasn't exactly 9 characters long -");
+				Console.WriteLine($"Expecting: [L01234567]");
+				Console.WriteLine($"Got: [{s.LNUMBER}]");
+			}
+
+			Console.WriteLine("\n");
+		}
+
+		static void TestStudentToString() {
+			Student s = new("L01234567", "John", "Smith", "Computer Programming", "1/01/1991");
+
+			Console.WriteLine("[Testing ToString]");
+
+			Console.WriteLine("Expecting: LNUMBER: [L01234567] | NameFirst: [John] | NameLast: [Smith] | Major: [Computer Programming] | BirthDate: [1/01/1991]");
+			Console.WriteLine($"Got: {s}\n");
+		}
+	}
 }
